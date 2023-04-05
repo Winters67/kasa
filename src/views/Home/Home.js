c40fef00ea92b5ca67124767857bbfdb24c83c23
Winters67 <emmanuel.vbg@gmail.com>
2023-04-05 18:17:38 +0200

    correction banner

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,13 +2,14 @@ import "../Home/Home.scss";
 import Banner from "../../components/Banner/Banner";
 
 function Home() {
- 
-
   return (
-    <div >
-      <Banner>
-      </Banner>
-    </div>
+    <section className="home-container">
+      <Banner></Banner>
+      
+     
+      
+      <div className="gallery">cgvbfgfrg</div>
+    </section>
   );
 }
 
